e966d8505694bd78956081aada3f2c20e3ccc519
Jetsung Chan <jetsungchan@gmail.com>
2025-07-30 16:01:30 +0800

    chore(ci): 更新 GitHub Action

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -25,7 +25,7 @@ const Footer: React.FC = () => {
         {
           key: 'Shortener GitHub',
           title: <GithubFilled />,
-          href: 'https://github.com/idevsig/shortener-server',
+          href: 'https://github.com/idev-sig/shortener-server',
           blankTarget: true,
         },
       ]}
